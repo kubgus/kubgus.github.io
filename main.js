1c7333354f0d7421bdf2a3661be37b5ce0b44f59
kubgus <53797257+kubgus@users.noreply.github.com>
2022-08-23 14:43:57 +0200

    UPGRADES, PEOPLE. UPGRADES.

--- main.js
+++ main.js
@@ -153,21 +153,37 @@ addGridSection("Other weird places you can find me:", [
 
 // }
 
+
+
 // BG parallax effect
+const elem = document.querySelector("#parallax");
+var auto = 0;
+let x = ``;
+let y = ``;
+
+function a() {
+    auto += 0.02;
+    if (auto > 999999) {
+        auto = 0
+    }
+    elem.style.backgroundPosition = `${auto + x}% ${y}%`;
+    window.requestAnimationFrame(a);
+}
+
+a();
+
 (function () {
     // Add event listener
     document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector("#parallax");
     // Magic happens here
     function parallax(e) {
         let _w = window.innerWidth / 2;
         let _h = window.innerHeight / 2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depth1 = `${50 + (_mouseX - _w) * 0.003}% ${50 + (_mouseY - _h) * 0.015}%`;
-        let x = `${_depth1}`;
+        x = 50 + (_mouseX - _w) * 0.003;
+        y = 50 + (_mouseY - _h) * 0.015;
         // console.log(x);
-        elem.style.backgroundPosition = x;
     }
 
 })();
