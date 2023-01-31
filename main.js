f70d749edf4298fa93a999e91cf875904fe932d0
kubgus <ja.gustafik@outlook.sk>
2023-01-31 16:37:44 +0100

    Fixis

--- main.js
+++ main.js
@@ -3,9 +3,5 @@ const RANDOM = document.getElementById("b2");
 const UP = document.getElementById("b3");
 
 UP.addEventListener("click", (e) => {
-    const sb = 50; // Scroll Bump
-    window.scrollTo(0, sb);
-    setTimeout(() => {
-        window.scrollTo(0, 0);
-    }, 200);
+    window.scrollTo(0, -1);
 });
