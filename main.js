9b6c29e36db813b9134965179f41acd98f5ea656
kubgus <ja.gustafik@outlook.sk>
2023-01-31 16:56:50 +0100

    Responsivity issues

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const UP = document.getElementById("b3");
 UP.addEventListener("click", (e) => {
     window.scrollTo(0, -1);
 });
+
+if (window.screen.width < 500) {
+    document.getElementsByTagName("h2")[0].addEventListener("click", () => {
+        window.scrollTo(0, 0);
+    });
+}
