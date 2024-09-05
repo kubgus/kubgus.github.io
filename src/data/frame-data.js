d840313aaa5f12f458f1ae6405be4879d01fba88
kubgus <ja.gustafik@outlook.sk>
2024-09-05 20:23:12 +0200

    Add popups for picture frames

---
+++ src/data/frame-data.js
@@ -0,0 +1,92 @@
+const techLogoPath = "images/tech-logos/";
+
+export default [
+    {
+        title: "Astro",
+        src: techLogoPath + "astro.png",
+    },
+    {
+        title: "AWS",
+        src: techLogoPath + "aws.png",
+    },
+    {
+        title: "C++",
+        src: techLogoPath + "cpp.png",
+    },
+    {
+        title: "C#",
+        src: techLogoPath + "cs.png",
+    },
+    {
+        title: "CSS",
+        src: techLogoPath + "css.png",
+    },
+    {
+        title: "Firebase",
+        src: techLogoPath + "firebase.png",
+    },
+    {
+        title: "Git",
+        src: techLogoPath + "git.png",
+    },
+    {
+        title: "GitHub",
+        src: techLogoPath + "github.png",
+    },
+    {
+        title: "HTML",
+        src: techLogoPath + "html.png",
+    },
+    {
+        title: "JavaScript",
+        src: techLogoPath + "javascript.png",
+    },
+    {
+        title: "Linux",
+        src: techLogoPath + "linux.png",
+    },
+    {
+        title: "MongoDB",
+        src: techLogoPath + "mongo.png",
+    },
+    {
+        title: "NeoVim",
+        src: techLogoPath + "neovim.png",
+    },
+    {
+        title: "Node.js",
+        src: techLogoPath + "node.png",
+    },
+    {
+        title: "Nuxt.js",
+        src: techLogoPath + "nuxt.png",
+    },
+    {
+        title: "Postgres",
+        src: techLogoPath + "postgres.png",
+    },
+    {
+        title: "Python",
+        src: techLogoPath + "python.png",
+    },
+    {
+        title: "Supabase",
+        src: techLogoPath + "supabase.png",
+    },
+    {
+        title: "TailwindCSS",
+        src: techLogoPath + "tailwind.png",
+    },
+    {
+        title: "TypeScript",
+        src: techLogoPath + "typescript.png",
+    },
+    {
+        title: "Unity",
+        src: techLogoPath + "unity.png",
+    },
+    {
+        title: "Vue.js",
+        src: techLogoPath + "vue.png",
+    },
+];
