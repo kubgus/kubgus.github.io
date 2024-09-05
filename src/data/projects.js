0486d40c102fc23ddcf3d409449425a7c36b274f
kubgus <ja.gustafik@outlook.sk>
2024-09-05 19:08:37 +0200

    Add data to projects.js (year, rating)

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,11 @@
+// Personal rating guideline:
+// 0 - No rating
+// 1 - Very old or very simple project
+// 2 - Clunky/unfinished project
+// 3 - Recent simple project or older larger project
+// 4 - Recent cool project
+// 5 - Recent impressive project
+
 export default [
     {
         title: "kubgus.github.io",
@@ -5,17 +13,23 @@ export default [
         website: "https://gustafik.com/",
         github: "https://github.com/kubgus/kubgus.github.io",
         image: "images/thumbnails/kubgus.github.io.png",
+        year: 2024,
+        rating: 4,
     },
     {
         title: "heystack",
         description: "Social media platform made with Nuxt.js and Supabase.",
         website: "https://heystak.netlify.app/",
+        year: 2023,
+        rating: 4,
     },
     {
         title: "bloodline",
         description: "SDL2 game engine written in C#.",
         github: "https://github.com/kubgus/bloodline",
         image: "images/thumbnails/bloodline.png",
+        year: 2023,
+        rating: 3,
     },
     {
         title: "omniroom",
@@ -23,6 +37,8 @@ export default [
         website: "https://play.gustafik.com/",
         github: "https://github.com/kubgus/omniroom",
         image: "images/thumbnails/omniroom.png",
+        year: 2023,
+        rating: 2,
     },
     {
         title: "filefly",
@@ -30,6 +46,8 @@ export default [
         github: "https://github.com/kubgus/filefly",
         website: "https://host.gustafik.com/",
         image: "images/thumbnails/filefly.png",
+        year: 2023,
+        rating: 3,
     },
     {
         title: "super tic-tac-toe",
@@ -37,6 +55,8 @@ export default [
         github: "https://github.com/kubgus/super-tic-tac-toe",
         website: "https://gustafik.com/super-tic-tac-toe",
         image: "images/thumbnails/super-tic-tac-toe.png",
+        year: 2023,
+        rating: 3,
     },
     {
         title: "tick oat two",
@@ -44,23 +64,31 @@ export default [
         github: "https://github.com/kubgus/tick-oat-two",
         website: "https://gustafik.com/tick-oat-two",
         image: "images/thumbnails/tick-oat-two.png",
+        year: 2022,
+        rating: 3,
     },
     {
         title: "magnet.io",
         description: "Fake .io game made for a collaborative game jam in Unity.",
         github: "https://github.com/kubgus/magnet-io",
         website: "https://laiq.itch.io/magnet-io",
+        year: 2022,
+        rating: 3,
     },
     {
         title: "copycat",
         description: "Relaxing puzzle game made for a collaborative game jam in Unity.",
         website: "https://mavelan.itch.io/copycat",
         image: "images/thumbnails/copycat.png",
+        year: 2022,
+        rating: 3,
     },
     {
         title: "discord tts bot",
         description: "Discord bot for text-to-speech made with Node.js and Discord.js.",
         github: "https://github.com/kubgus/discord-tts-bot",
+        year: 2022,
+        rating: 2,
     },
     {
         title: "flashcards",
@@ -68,70 +96,118 @@ export default [
         github: "https://github.com/kubgus/flashcards",
         website: "https://gustafik.com/flashcards",
         image: "images/thumbnails/flashcards.png",
+        year: 2023,
+        rating: 1,
     },
     {
         title: "stratus",
         description: "Port forwarding service made with Express.js and Supabase.",
         github: "https://github.com/kubgus/stratus",
         website: "https://mrkd.dev/",
+        year: 2024,
+        rating: 4,
     },
     {
         title: "stratus cli",
         description: "CLI for stratus made with Node.js.",
         github: "https://github.com/kubgus/stratus-cli",
+        year: 2024,
+        rating: 4,
     },
     {
         title: "untitled game engine",
         description: "SDL2 game engine made with C++.",
         github: "https://github.com/kubgus/untitled-game-engine",
+        year: 2024,
+        rating: 0,
     },
     {
         title: "slidar",
         description: "Slideshow presentation tool for markdown made with Node.js.",
         github: "https://github.com/kubgus/slidar",
+        year: 2024,
+        rating: 3,
     },
     {
         title: "texemble",
         description: "Terminal game engine made with C++.",
         github: "https://github.com/kubgus/texemble",
+        year: 2024,
+        rating: 4,
     },
     {
         title: "kudo",
         description: "My first 2D game engine made with C#.",
         github: "https://github.com/kubgus/kudo",
         image: "images/thumbnails/kudo.png",
+        year: 2022,
+        rating: 3,
     },
     {
         title: "frisk",
         description: "Advanced ls tool for Linux made with C++.",
         github: "https://github.com/kubgus/frisk",
+        year: 2024,
+        rating: 4,
     },
     {
         title: "hotpot",
         description: "Hot reload tool for any language made with C++.",
         github: "https://github.com/kubgus/hotpot",
+        year: 2024,
+        rating: 4,
     },
     {
         title: "c++ cheatsheet",
         description: "Collection of advanced C++ concepts and snippets.",
         github: "https://github.com/kubgus/cpp-cheatsheet",
+        year: 2023,
+        rating: 4,
     },
     {
         title: "overwork",
         description: "Chrome extension that reminds you to take breaks.",
         website: "https://chromewebstore.google.com/detail/overwork/jhpbphonlhcgjieobohcpamgejbbkkcj",
         image: "images/thumbnails/overwork.png",
+        year: 2023,
+        rating: 4,
     },
     {
         title: "faux",
         description: "Chrome extension that generates fake user data and working temporary emails.",
         website: "https://chromewebstore.google.com/detail/faux/kffcggckolacihcclnifefneifapjcdl",
         image: "images/thumbnails/faux.png",
+        year: 2023,
+        rating: 5,
     },
     {
         title: "instant",
         description: "Secure chatboard website made with Express.js and Socket.io.",
         website: "https://instant.gustafik.com/",
         image: "images/thumbnails/instant.png",
+        year: 2022,
+        rating: 2,
+    },
+    {
+        title: "scratch studio",
+        description: "My studio on Scratch.",
+        website: "https://scratch.mit.edu/studios/29203310/",
+        year: 2020,
+        rating: 1,
+    },
+    {
+        title: "planet generator",
+        description: "A simple website that uses the canvas element to generate planets.",
+        website: "https://gustafik.com/planet-generator",
+        image: "images/thumbnails/planet-generator.png",
+        year: 2021,
+        rating: 1,
+    },
+    {
+        title: "speed read",
+        description: "A basic HTML website for speed reading.",
+        website: "https://gustafik.com/speed-read",
+        year: 2021,
+        rating: 1,
     },
 ];
